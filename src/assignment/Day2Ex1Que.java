cac3e28a43bf155f8134833e49ed5bbe42af0111
YashashriJadhav <YashashriJadhav@DESKTOP-G6MS6PD>
2020-07-06 12:49:03 +0530

    Practice assignment

--- src/assignment/Day2Ex1Que.java
+++ src/assignment/Day2Ex1Que.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Day2Ex1Que {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
